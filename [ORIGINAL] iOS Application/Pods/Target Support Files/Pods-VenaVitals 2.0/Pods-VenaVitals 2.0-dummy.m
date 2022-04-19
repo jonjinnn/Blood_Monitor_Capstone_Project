f7175b7187a83d46843ba5215cc0ecaf93fca093
jonjinn <jonjin91@gmail.com>
2022-04-19 14:46:52 -0700

    update
    update backup

--- [ORIGINAL] iOS Application/Pods/Target Support Files/Pods-VenaVitals 2.0/Pods-VenaVitals 2.0-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VenaVitals_2_0 : NSObject
-@end
-@implementation PodsDummy_Pods_VenaVitals_2_0
-@end
--- [ORIGINAL] iOS Application/build/Pods.build/Debug-iphonesimulator/XMLCoder.build/Objects-normal/x86_64/XMLCoder_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/Mustafa/Documents/GitHub/venavitals_cbp_ios/build/Pods.build/Debug-iphonesimulator/XMLCoder.build/DerivedSources/XMLCoder_vers.c
